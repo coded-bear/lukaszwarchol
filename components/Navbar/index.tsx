f745c7629dd7d3b9054cc78907ab2ef18a024f4d
Łukasz Warchoł <lukasz.warchol14@gmail.com>
2019-12-24 21:56:23 +0100

    added navbar

---
+++ components/Navbar/index.tsx
@@ -0,0 +1,18 @@
+import React from "react";
+import Link from "next/link";
+
+const Navbar: React.FC = () => (
+  <header>
+    <Link href="" as="" prefetch={false}>
+      <a>{/* logo */}</a>
+    </Link>
+
+    <div>
+      <div>{/* lang checking */}</div>
+      <div>{/* mode checking */}</div>
+      <button>{/* navbtn */}</button>
+    </div>
+  </header>
+);
+
+export default Navbar;
