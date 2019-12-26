e966fd7260dcc1374507b0559990ca3a5b19090c
Łukasz Warchoł <lukasz.warchol14@gmail.com>
2019-12-26 01:52:48 +0100

    changed style for footer

--- styled/footer.ts
+++ styled/footer.ts
@@ -1,7 +1,19 @@
 import styled from "styled-components";
 
 export const StyledFooter = styled.footer`
+  text-align: center;
+
   nav {
     font-size: 22px;
+
+    ul {
+      display: flex;
+
+      li {
+        padding: 5px 10px;
+        align-items: center;
+        justify-content: center;
+      }
+    }
   }
 `;
