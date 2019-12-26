b7609f6d728131d57dabc6874d8a6ddf2b3b7c95
Łukasz Warchoł <lukasz.warchol14@gmail.com>
2019-12-26 19:47:53 +0100

    added subtitles to pages

--- components/Projects/index.tsx
+++ components/Projects/index.tsx
@@ -1,12 +1,13 @@
 import React from "react";
-import { Title, Heading } from "../elements";
+import { Title, Heading, SubTitle } from "../elements";
 
 const Projects: React.FC = () => (
-  <>
+  <section>
     <Title text="Projects" />
 
     <Heading text="Projects" />
-  </>
+    <SubTitle text="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation" />
+  </section>
 );
 
 export default Projects;
