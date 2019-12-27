7911dc869ba5a76079839c78f1ae24c9f3b95448
Łukasz Warchoł <lukasz.warchol14@gmail.com>
2019-12-27 17:18:32 +0100

    added images for questions

--- components/Home/HomeQuestions/index.tsx
+++ components/Home/HomeQuestions/index.tsx
@@ -2,6 +2,10 @@ import React from "react";
 import { LinkButton } from "components/elements";
 import { QuestionElemProps } from "../interfaces";
 
+import imgPerson from "../../../static/images/home/person.svg";
+import imgBulb from "../../../static/images/home/bulb.svg";
+import imgRocket from "../../../static/images/home/rocket.svg";
+
 const QuestionElem: React.FC<QuestionElemProps> = props => (
   <div>
     <img src={props.image} alt={props.title} />
@@ -15,21 +19,21 @@ const QuestionElem: React.FC<QuestionElemProps> = props => (
 const HomeQuestions: React.FC = () => (
   <section>
     <QuestionElem
-      image="image"
+      image={imgPerson}
       title="Who am I?"
       description="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat."
       href="/about"
       as="/about"
     />
     <QuestionElem
-      image="image"
+      image={imgBulb}
       title="What I do?"
       description="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat."
       href="/services"
       as="/services"
     />
     <QuestionElem
-      image="image"
+      image={imgRocket}
       title="What have I done?"
       description="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat."
       href="/projects"
