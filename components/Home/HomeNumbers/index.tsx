2ad5c2a8d2bf590f0e5046bb2fb150604860bd0d
Łukasz Warchoł <lukasz.warchol14@gmail.com>
2019-12-27 17:23:14 +0100

    added images for numbers

--- components/Home/HomeNumbers/index.tsx
+++ components/Home/HomeNumbers/index.tsx
@@ -1,6 +1,10 @@
 import React from "react";
 import { NumbersElemProps } from "../interfaces";
 
+import imgCaffee from "../../../static/images/home/caffee.svg";
+import imgCode from "../../../static/images/home/code.svg";
+import imgLightning from "../../../static/images/home/lightning.svg";
+
 const NumbersElem: React.FC<NumbersElemProps> = props => (
   <div>
     <img src={props.image} alt={props.title} />
@@ -13,9 +17,9 @@ const NumbersElem: React.FC<NumbersElemProps> = props => (
 
 const HomeNumbers: React.FC = () => (
   <section>
-    <NumbersElem image="image" title="Wypitych kaw" number="1 500+" />
-    <NumbersElem image="image" title="Linijek kodu" number="1 000 000+" />
-    <NumbersElem image="image" title="Poznanych technologii" number="20+" />
+    <NumbersElem image={imgCaffee} title="Wypitych kaw" number="1 500+" />
+    <NumbersElem image={imgCode} title="Linijek kodu" number="1 000 000+" />
+    <NumbersElem image={imgLightning} title="Poznanych technologii" number="20+" />
   </section>
 );
 
