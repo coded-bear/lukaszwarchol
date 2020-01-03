d8bfa6cea2b598a6d6154c6f5a3e788597f11bbb
Łukasz Warchoł <lukasz.warchol14@gmail.com>
2020-01-03 17:16:26 +0100

    added ga code and icons links to constants

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,5 +1,6 @@
 import Document, { Html, Head, Main, NextScript } from "next/document";
 import { ServerStyleSheet } from "styled-components";
+import { GA_CODE, ICONS_LINKS } from "../utils/constants";
 
 interface Props {
   main: any;
@@ -27,11 +28,7 @@ class MyDocument extends Document<Props> {
           <link rel="icon" href="/static/images/favicon.ico" />
           <link href="https://fonts.googleapis.com/css?family=Raleway:400,700&display=swap" rel="stylesheet"></link>
 
-          <script
-            dangerouslySetInnerHTML={{
-              __html: `(function(i,s,o,g,r,a,m){i["GoogleAnalyticsObject"]=r;(i[r]=i[r]||function(){(i[r].q=i[r].q||[]).push(arguments);}),(i[r].l=1*new Date());(a=s.createElement(o)),(m=s.getElementsByTagName(o)[0]);a.async=1;a.src=g;m.parentNode.insertBefore(a,m);})(window,document,"script","https://www.google-analytics.com/analytics.js","ga");ga("create","UA-97392728-1","auto");ga("send","pageview");`
-            }}
-          />
+          <script dangerouslySetInnerHTML={{ __html: GA_CODE }} />
 
           {styleTags}
         </Head>
@@ -40,11 +37,7 @@ class MyDocument extends Document<Props> {
           <NextScript />
         </body>
 
-        <script
-          dangerouslySetInnerHTML={{
-            __html: `<!--<div>Icons made by <a href="https://www.flaticon.com/authors/dave-gandy" title="Dave Gandy">Dave Gandy</a> from <a href="https://www.flaticon.com/" title="Flaticon">www.flaticon.com</a></div><div>Icons made by <a href="https://www.flaticon.com/authors/freepik" title="Freepik">Freepik</a> from <a href="https://www.flaticon.com/" title="Flaticon">www.flaticon.com</a></div><div>Icons made by <a href="https://www.flaticon.com/authors/freepik" title="Freepik">Freepik</a> from <a href="https://www.flaticon.com/" title="Flaticon">www.flaticon.com</a></div><div>Icons made by <a href="https://www.flaticon.com/authors/freepik" title="Freepik">Freepik</a> from <a href="https://www.flaticon.com/" title="Flaticon">www.flaticon.com</a></div><div>Icons made by <a href="https://www.flaticon.com/authors/smashicons" title="Smashicons">Smashicons</a> from <a href="https://www.flaticon.com/" title="Flaticon">www.flaticon.com</a></div><div>Icons made by <a href="https://www.flaticon.com/authors/freepik" title="Freepik">Freepik</a> from <a href="https://www.flaticon.com/" title="Flaticon">www.flaticon.com</a></div><div>Icons made by <a href="https://www.flaticon.com/authors/gregor-cresnar" title="Gregor Cresnar">Gregor Cresnar</a> from <a href="https://www.flaticon.com/" title="Flaticon">www.flaticon.com</a></div>-->`
-          }}
-        />
+        <script dangerouslySetInnerHTML={{ __html: ICONS_LINKS }} />
       </Html>
     );
   }
