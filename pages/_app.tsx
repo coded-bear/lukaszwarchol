620e93e6c0364881705ebfeb375522b01c3ccfdc
Łukasz Warchoł <lukasz.warchol14@gmail.com>
2020-01-03 16:33:37 +0100

    added GA code

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,3 +1,4 @@
+import React from "react";
 import App from "next/app";
 import Layout from "../Layout";
 import { getLang } from "../utils/langService";
--- pages/_document.tsx
+++ pages/_document.tsx
@@ -27,6 +27,12 @@ class MyDocument extends Document<Props> {
           <link rel="icon" href="/static/images/favicon.ico" />
           <link href="https://fonts.googleapis.com/css?family=Raleway:400,700&display=swap" rel="stylesheet"></link>
 
+          <script
+            dangerouslySetInnerHTML={{
+              __html: `(function(i,s,o,g,r,a,m){i["GoogleAnalyticsObject"]=r;(i[r]=i[r]||function(){(i[r].q=i[r].q||[]).push(arguments);}),(i[r].l=1*new Date());(a=s.createElement(o)),(m=s.getElementsByTagName(o)[0]);a.async=1;a.src=g;m.parentNode.insertBefore(a,m);})(window,document,"script","https://www.google-analytics.com/analytics.js","ga");ga("create","UA-97392728-1","auto");ga("send","pageview");`
+            }}
+          />
+
           {styleTags}
         </Head>
         <body>
