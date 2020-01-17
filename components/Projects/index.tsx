7fab3dfcb83aac06fd276d4b9d400d0aa5cb56b3
Łukasz Warchoł <lukasz.warchol14@gmail.com>
2020-01-17 19:22:46 +0100

    added buttons to projects

--- components/Projects/index.tsx
+++ components/Projects/index.tsx
@@ -1,16 +1,40 @@
-import React from "react";
+import React, { useState, useCallback, memo } from "react";
 import { NextPage } from "next";
+import { Container } from "../../styled/layout";
 import { getLang } from "../../utils/langService";
 import { Title, Heading, SubTitle } from "../elements";
+import { ButtonProps } from "./interfaces";
 
-const Projects: NextPage<{ t: any }> = ({ t }) => (
-  <section>
-    <Title text={t.title} />
+const Button = memo((props: ButtonProps) => <button onClick={props.onClick}>{props.name}</button>);
 
-    <Heading text={t.title} />
-    <SubTitle text={t.subtitle} />
-  </section>
-);
+const Projects: NextPage<{ t: any }> = ({ t }) => {
+  const [view, setView] = useState<number>(1);
+
+  const changeView = useCallback(
+    (nr: number) => {
+      setView(nr);
+    },
+    [view]
+  );
+
+  return (
+    <section>
+      <Title text={t.title} />
+
+      <Heading text={t.title} />
+      <SubTitle text={t.subtitle} />
+
+      <Container>
+        <div>
+          <Button onClick={() => changeView(1)} name="All" />
+          <Button onClick={() => changeView(2)} name="WEB Applications" />
+          <Button onClick={() => changeView(3)} name="Websites" />
+          <Button onClick={() => changeView(4)} name="Mockups" />
+        </div>
+      </Container>
+    </section>
+  );
+};
 
 Projects.getInitialProps = async ({ asPath }) => {
   const lang: string = await getLang(asPath);
