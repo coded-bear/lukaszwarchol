c1992a6835a18dab5a15521314e901ab34193155
Łukasz Warchoł <lukasz.warchol14@gmail.com>
2020-02-21 21:22:55 +0100

    changed / page

--- gatsby-node.js
+++ gatsby-node.js
@@ -4,6 +4,15 @@ exports.createPages = ({ graphql, actions }) => {
   const { createPage } = actions;
   const locales = ["pl", "en"];
 
+  createPage({
+    path: `/`,
+    component: path.resolve("src/components/Home/index.jsx"),
+    context: {
+      lang: `pl`,
+      t: require(`./src/locales/pl/home.json`)
+    }
+  });
+
   locales.map(locale => {
     createPage({
       path: `/${locale}/`,
--- src/pages/index.jsx
+++
@@ -1,3 +0,0 @@
-import React from "react";
-
-export default () => <h1>Welcome to my new portfolio written in Gatsby</h1>;
