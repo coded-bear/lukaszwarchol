a3398715f08c3df25cf06f693f181d9dce67a842
Łukasz Warchoł <lukasz.warchol14@gmail.com>
2020-02-23 20:11:15 +0100

    added navigation

---
+++ src/components/navigation.jsx
@@ -0,0 +1,36 @@
+import React, { forwardRef } from "react";
+import { Link } from "gatsby";
+import { SocialMedia } from "./elements";
+import { StyledNavigation, StyledNavigationSocialMedia } from "../styled/header";
+
+import imgClose from "../images/common/close.svg";
+
+const NavElem = ({ to, text, closeMenu }) => (
+  <Link to={to}>
+    <li onClick={closeMenu}>{text}</li>
+  </Link>
+);
+
+const Navigation = forwardRef(({ lang, t, closeMenu }, ref) => (
+  <StyledNavigation ref={ref}>
+    <button onClick={closeMenu}>
+      <img src={imgClose} alt="close" />
+    </button>
+
+    <nav>
+      <ul>
+        <NavElem to={`/${lang}/`} text="Home" closeMenu={closeMenu} />
+        <NavElem to={`/${lang}/about-me/`} text={t.nav.about} closeMenu={closeMenu} />
+        <NavElem to={`/${lang}/services/`} text={t.nav.services} closeMenu={closeMenu} />
+        <NavElem to={`/${lang}/projects/`} text={t.nav.projects} closeMenu={closeMenu} />
+        <NavElem to={`/${lang}/contact/`} text={t.nav.contact} closeMenu={closeMenu} />
+      </ul>
+    </nav>
+
+    <StyledNavigationSocialMedia>
+      <SocialMedia />
+    </StyledNavigationSocialMedia>
+  </StyledNavigation>
+));
+
+export default Navigation;
