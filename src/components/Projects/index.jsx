ee934cc23888db7851ce98c6907b4f739b99fb84
Łukasz Warchoł <lukasz.warchol14@gmail.com>
2020-03-08 23:57:10 +0100

    removed projects content

--- src/components/Projects/index.jsx
+++ src/components/Projects/index.jsx
@@ -1,20 +1,10 @@
-import React, { useState } from "react";
+import React from "react";
 import PropTypes from "prop-types";
-import "../../styles/projects.scss";
 import SEO from "../seo";
 import Layout from "../layout";
 import { Heading, SubTitle } from "../elements";
-import { All, WebApplications, Websites, Mockups } from "./AllProjects";
-
-const Button = ({ onClick, active, name }) => (
-  <button className={`Projects__btn ${active && "active"}`} onClick={onClick}>
-    {name}
-  </button>
-);
 
 const Projects = props => {
-  const [view, setView] = useState(1);
-
   const { lang, t, layoutT } = props.pageContext;
   const { path } = props;
 
@@ -26,34 +16,10 @@ const Projects = props => {
         <Heading text={t.title} />
         <SubTitle text={t.subtitle} />
 
-        <div className="container">
-          <div>
-            {t.views.map((elem, index) => (
-              <Button key={index} onClick={() => setView(index + 1)} active={view === index + 1} name={elem} />
-            ))}
-          </div>
-
-          <div className="Projects__list">
-            {view === 2 ? (
-              <WebApplications path={path} />
-            ) : view === 3 ? (
-              <Websites path={path} />
-            ) : view === 4 ? (
-              <Mockups path={path} />
-            ) : (
-              <All path={path} />
-            )}
-          </div>
-        </div>
+        <div className="container"></div>
       </section>
     </Layout>
   );
-};
-
-Button.propTypes = {
-  onClick: PropTypes.func.isRequired,
-  active: PropTypes.bool.isRequired,
-  name: PropTypes.string.isRequired
 };
 
 Projects.propTypes = {
