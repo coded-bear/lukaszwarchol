381e3ff81e277af8176c5a4c3345d3685f5cfac7
Łukasz Warchoł <lukasz.warchol14@gmail.com>
2020-03-25 23:18:12 +0100

    changed cookies info

--- src/components/CookiesInfo.jsx
+++ src/components/CookiesInfo.jsx
@@ -4,13 +4,12 @@ import { Link } from "gatsby";
 import { getHrefs } from "../utils/langService";
 import imgClose from "../images/common/close.svg";
 
-const CookiesInfo = ({ lang, t }) => (
+const CookiesInfo = ({ lang, t, close }) => (
   <div className="CookiesInfo">
     <div className="container">
-      <button className="CookiesInfo__close">
+      <button onClick={close} className="CookiesInfo__close">
         <img src={imgClose} alt="close" />
       </button>
-
       <p>
         {t.text} <Link to={`/${lang}/${getHrefs(lang)[4]}/`}>{t.privacyPolicy}</Link>
       </p>
@@ -20,7 +19,8 @@ const CookiesInfo = ({ lang, t }) => (
 
 CookiesInfo.propTypes = {
   lang: PropTypes.string.isRequired,
-  t: PropTypes.object.isRequired
+  t: PropTypes.object.isRequired,
+  close: PropTypes.func.isRequired
 };
 
 export default CookiesInfo;
--- src/components/layout.jsx
+++ src/components/layout.jsx
@@ -1,26 +1,42 @@
-import React from "react";
+import React, { useEffect } from "react";
 import PropTypes from "prop-types";
 import "../styles/index.scss";
 import Header from "./Header";
 import { HireMeBox } from "./elements";
 import Footer from "./Footer";
 import CookiesInfo from "./CookiesInfo";
+import Cookies from "js-cookie";
+import { useState } from "react";
 
-const Layout = ({ children, lang, path, t }) => (
-  <>
-    <Header lang={lang} path={path} t={t} />
+const Layout = ({ children, lang, path, t }) => {
+  const [cookiesInfo, setCookiesInfo] = useState(false);
 
-    <main>
-      {children}
+  useEffect(() => {
+    const ca = Cookies.get("ca");
+    if (!ca) setCookiesInfo(true);
+  }, []);
 
-      <HireMeBox lang={lang} t={t} />
-    </main>
+  const closeCookiesInfo = () => {
+    Cookies.set("ca", true, { expires: 365 });
+    setCookiesInfo(false);
+  };
 
-    <Footer lang={lang} t={t} />
+  return (
+    <>
+      <Header lang={lang} path={path} t={t} />
 
-    <CookiesInfo lang={lang} t={t.cookiesInfo} />
-  </>
-);
+      <main>
+        {children}
+
+        <HireMeBox lang={lang} t={t} />
+      </main>
+
+      <Footer lang={lang} t={t} />
+
+      {cookiesInfo && <CookiesInfo lang={lang} t={t.cookiesInfo} close={closeCookiesInfo} />}
+    </>
+  );
+};
 
 Layout.propTypes = {
   children: PropTypes.node.isRequired,
