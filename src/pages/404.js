9d19f06e855534f3ce796effad36f280a8467bcc
Łukasz Warchoł <lukasz.warchol@fabrity.pl>
2020-07-08 20:32:16 +0200

    added style for not found page

---
+++ src/pages/404.js
@@ -0,0 +1,26 @@
+import React from "react";
+import "../styles/notFoundPage.scss";
+import SEO from "../components/seo";
+import Layout from "../components/layout";
+import imgNotFound from "../images/common/not-found.svg";
+import layoutT from "../static/locales/pl/layout.json";
+import { LinkButton } from "../components/elements";
+
+const NotFoundPage = ({ path }) => (
+  <Layout t={layoutT} path={path} lang="pl">
+    <section className="NotFoundPage">
+      <SEO title="Nie znaleziono strony" />
+
+      <div className="container NotFoundPage__container">
+        <div className="NotFoundPage__content">
+          <h1>404</h1>
+          <h2>Nie znaleziono strony</h2>
+          <LinkButton to="/pl/" text="Przejdź do strony głównej" />
+        </div>
+        <img src={imgNotFound} alt="Nie znaleziono" />
+      </div>
+    </section>
+  </Layout>
+);
+
+export default NotFoundPage;
