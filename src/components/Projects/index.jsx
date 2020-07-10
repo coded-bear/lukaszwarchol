2311b8290528ee4f43018c53c2ed85bb997c0631
Łukasz Warchoł <lukasz.warchol@fabrity.pl>
2020-07-10 12:09:51 +0200

    changed proejcts page

--- src/components/Projects/index.jsx
+++ src/components/Projects/index.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import PropTypes from "prop-types";
 import SEO from "../seo";
 import "../../styles/pages/projects.scss";
+import { Link } from "gatsby";
 import MainLayout from "../../layouts/MainLayout";
 import Subtitle from "../common/Subtitle";
 import Heading from "../common/Heading";
@@ -23,8 +24,29 @@ const Projects = props => {
             <div key={index} className="Project">
               <div className={`Project__image ${project.shortName}`}></div>
               <div className="Project__content">
-                <h3 className="name">{project.name}</h3>
-                <p>{project.description}</p>
+                <div>
+                  <h3 className="name">{project.name}</h3>
+                  <p>{project.description}</p>
+                </div>
+
+                <div className="Project__links">
+                  {project.links.github && (
+                    <a href={project.links.github} target="_blank" rel="noopener noreferrer">
+                      GitHub
+                    </a>
+                  )}
+                  {project.links.behance && (
+                    <a href={project.links.behance} target="_blank" rel="noopener noreferrer">
+                      Behance
+                    </a>
+                  )}
+                  {project.links.learnMore && <Link to={`${path + project.links.learnMore}/`}>{t.learnMore}</Link>}
+                  {project.links.go && (
+                    <a href={project.links.go} target="_blank" rel="noopener noreferrer">
+                      {t.go}
+                    </a>
+                  )}
+                </div>
               </div>
             </div>
           ))}
