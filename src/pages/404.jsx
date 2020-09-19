edb722a53348c57b337d505acbb7d056a422b7bd
Łukasz Warchoł <lukasz.warchol@fabrity.pl>
2020-09-19 17:56:00 +0200

    changed not found page

--- src/pages/404.jsx
+++ src/pages/404.jsx
@@ -3,7 +3,9 @@ import "../styles/pages/notFoundPage.scss";
 import SEO from "../components/seo";
 import MainLayout from "../layouts/MainLayout";
 import LinkButton from "../components/common/LinkButton";
+
 import imgNotFound from "../images/common/not-found.svg";
+
 import layoutT from "../static/locales/pl/layout.json";
 
 const NotFoundPage = ({ path }) => (
@@ -15,8 +17,9 @@ const NotFoundPage = ({ path }) => (
         <div className="NotFoundPage__content">
           <h1>404</h1>
           <h2>Nie znaleziono strony</h2>
-          <LinkButton to="/pl/" text="Przejdź do strony głównej" />
+          <LinkButton to="/pl/">Przejdź do strony głównej</LinkButton>
         </div>
+
         <img src={imgNotFound} alt="Nie znaleziono" />
       </div>
     </section>
